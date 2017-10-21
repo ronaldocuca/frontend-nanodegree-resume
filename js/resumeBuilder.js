d09ffbe785e02c20cd6c94db83b9fd3a196966b6
Ronaldo Rocha <ronaldocuca@gmail.com>
2017-10-21 20:37:33 -0200

    add header

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,7 +1,18 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
- $("#main").append("Ronaldo Rocha");
+ // $("#main").append("Ronaldo Rocha");
 
- var awesomeThoughts = "I am Ronaldo Rocha and I am AWESOME!";
+ // var awesomeThoughts = "I am Ronaldo Rocha and I am AWESOME!";
+ // var funThoughts = awesomeThoughts.replace("AWESOME","FUN");
+ 
  console.log(awesomeThoughts);
+ console.log(funThoughts);
+
+ // $("#main").append(funThoughts);
+
+ var formattedName = HTMLheaderName.replace("%data%","Ronaldo Rocha");
+ var formattedRole = HTMLheaderRole.replace("%data%","Web Developer fullstack");
+
+ $("#header").append(formattedName);
+ $("#header").append(formattedRole);
