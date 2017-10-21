d5e1a5b1d94d605d691789280e584e76c8b9557c
Ronaldo Rocha <ronaldocuca@gmail.com>
2017-10-21 19:55:47 -0200

    add variable

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,4 +1,7 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
- $('#main').append('Ronaldo Silva Rocha');
+ $("#main").append("Ronaldo Rocha");
+
+ var awesomeThoughts = "I am Ronaldo Rocha and I am AWESOME!";
+ console.log(awesomeThoughts);
